aeaf37ed05536eef8e9e59cea89d93965bc8f188
Egglol1 <24egglal@gmail.com>
2024-12-12 15:53:23 -0500

    Removed redundant text

--- src/index.jsx
+++ src/index.jsx
@@ -8,7 +8,7 @@ import "./index.scss";
 // Main component (will eventually use all the others)
 const MyFlixApplication = () => {
   <Container>
-  return <MainView />
+   <MainView />
   </Container>
 };
 // Finds the root of your app
