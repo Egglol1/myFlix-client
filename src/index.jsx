81200e145c35fe4cc5cdbd0e10c41222a6f95a80
Egglol1 <24egglal@gmail.com>
2024-12-12 15:53:10 -0500

    Placed MainView in a Container

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,15 @@
 import { createRoot } from 'react-dom/client';
 import { MainView } from './components/main-view/main-view';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from 'react-bootstrap/Container';
 // Import statement to indicate that you need to bundle `./index.scss`
 import "./index.scss";
 
 // Main component (will eventually use all the others)
 const MyFlixApplication = () => {
+  <Container>
   return <MainView />
+  </Container>
 };
 // Finds the root of your app
 const container = document.querySelector("#root");
