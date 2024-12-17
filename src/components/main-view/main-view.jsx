7051b25b530b0f8b791d2275dde4ff2fffc849db
Egglol1 <24egglal@gmail.com>
2024-12-17 10:08:46 -0500

    Imported Row from bootstrap-react

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -4,6 +4,7 @@ import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
 import { SignupView } from "../signup-view/signup-view";
 import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 export const MainView = () => {
   const storedUser = JSON.parse(localStorage.getItem("user"));
