e893fdbc953c00ce7cdab88d9ba55d82b8ffe682
Egglol1 <24egglal@gmail.com>
2024-12-20 20:16:14 -0500

    Removed part of error message that was, itself, causing errors

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -23,7 +23,7 @@ export const LoginView = ({ onLoggedIn }) => {
       body: JSON.stringify(data)
     }).then((response) => response.json())
       .then((data) => {
-      console.log("Login response: ", data, "Response :", response);
+      console.log("Login response: ", data);
       if (data.user) {
         localStorage.setItem("user", JSON.stringify(data.user));
         localStorage.setItem("token", data.token);
