131579c075858aa6553f08c2b6cb41c1bbdd6bc4
Egglol1 <24egglal@gmail.com>
2024-12-22 19:44:59 -0500

    Changed components to work with new additions

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,15 +3,16 @@ import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
 import { SignupView } from "../signup-view/signup-view";
+import { NavigationBar } from "../navigation-bar/navigation-bar";
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 export const MainView = () => {
   console.log(localStorage.getItem("user"));
   const storedUser = JSON.parse(localStorage.getItem("user"));
   const storedToken = localStorage.getItem("token");
   const [movies, setMovies] = useState([]);
-  const [selectedMovie, setSelectedMovie] = useState(null);
   const [user, setUser] = useState(storedUser);
   const [token, setToken] = useState(storedToken);
 
@@ -39,43 +40,82 @@ export const MainView = () => {
   }, [token]);
   
     return (
-      <Row>
-        {!user ? (
-          <Col md={5}>
-            <LoginView onLoggedIn={(user) => setUser(user)} />
-            or
-            <SignupView />
-          </Col>
-        ) : selectedMovie ? (
-          <Col md={8}>
-            <MovieView
-            movie={selectedMovie}
-            onBackClick={() => setSelectedMovie(null)}
+      <BrowserRouter>
+      <NavigationBar
+        user={user}
+        onLoggedOut={() => {
+          setUser(null);
+        }}
+      />
+        <Row>
+          <Routes>
+            <Route
+              path="/signup"
+              element={
+                <>
+                {user ? (
+                  <Navigate to="/" />
+                ) : (
+                  <Col md={5}>
+                    <SignupView/>
+                  </Col>
+                )}
+                </>
+              }
             />
-          </Col>
-        ) : (movies.length === 0) ? (
-          <Col>
-          The list is empty!
-          </Col>
-        ) : (
-          <>
-          <Col xs={12}>
-            <h1>Movie List!</h1>
-          </Col>
-          {movies.map((movie) => (
-            <Col className="mb-5" key={movie.id} md={3}>
-              <MovieCard
-              key={movie.id}
-              movie={movie}
-              onMovieClick={(newSelectedMovie) => {
-                setSelectedMovie(newSelectedMovie);
-              }}
-              />
-            </Col>
-          ))}
-          </>
-        )}
-        <button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</button>
-      </Row>
+            <Route
+            path="/login"
+            element={
+              <>
+                {user ? (
+                  <Navigate to="/" />
+                ) : (
+                  <Col md={5}>
+                    <LoginView onLoggedIn={(user) => setUser(user)} />
+                  </Col>
+                )}
+              </>
+
+            }
+            />
+          <Route
+            path="movies/:movieId"
+            element={
+              <>
+              {!user ? (
+                <Navigate to="/login" replace />
+              ) : movies.length === 0? (
+                <Col>The list is Empty!</Col>
+              ) : (
+                <Col md={8}>
+                  <MovieView movies={movies} />
+                </Col>
+              )}
+              </>
+            }
+          />
+          <Route
+            path="/"
+            element={
+              <>
+              {!user ? (
+                <Navigate to="/login" replace />
+              ) : movies.length === 0 ? (
+                <Col>The list is empty!</Col>
+              ) : (
+                <>
+                  {movies.map((movie) => (
+                    <Col className="mb-4" key={movie.id} md={3}>
+                      <MovieCard movie={movie} />
+                    </Col>
+                  ))}
+                </>
+              )}
+              </>
+            }
+          />
+          </Routes>
+        </Row>
+      </BrowserRouter>
   );
   };
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,17 +1,32 @@
 import PropTypes from "prop-types";
 import { Button, Card } from "react-bootstrap";
+import React, {useState} from "react";
+import { Link } from "react-router-dom";
 
 import "./movie-card.scss";
 
-export const MovieCard = ({ movie, onMovieClick }) => {
+export const MovieCard = ({ movie, isFavorite, onFavoriteToggle }) => {
+  const [isFav, setIsFav] = useState(isFavorite);
+
+  const handleFavoriteClick = () => {
+    setIsFav(prev => !prev);
+    onFavoriteToggle(movie._id, !isFav);
+  };
+
   return (
     <Card clasName="h-100">
       <Card.Img variant="top" src={movie.image} />
       <Card.Body>
         <Card.Title>{movie.title}</Card.Title>
         <Card.Text>{movie.author}</Card.Text>
-        <Button onClick={() => onMovieClick(movie)} variant="link">
-          Open
+        <Link to={`/movies/${encodeURIComponent(movie.id)}`}>
+          <Button variant="link">Open</Button>
+        </Link>
+        <Button
+          variant={isFav ? "danger" : "secondary"}
+          onClick={handleFavoriteClick}
+        >
+          {isFav ? "Unfavorite" : "Favorite"}
         </Button>
       </Card.Body>
     </Card>
@@ -24,5 +39,6 @@ MovieCard.propTypes = {
     image: PropTypes.string.isRequired,
     director: PropTypes.string
   }).isRequired,
-  onMovieClick: PropTypes.func.isRequired
+  isFavorite: PropTypes.bool.isRequired,
+  onFavoriteToggle: PropTypes.func.isRequired
 };
