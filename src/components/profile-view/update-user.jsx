9a1ae8816aa41882e3ecc9565c5975278957b245
Egglol1 <24egglal@gmail.com>
2025-01-06 22:04:21 -0500

    Fixed importing button twice

--- src/components/profile-view/update-user.jsx
+++ src/components/profile-view/update-user.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import { Button } from "react-bootstrap";
 
 export const UpdateUser =({user, handleChange, handleSaveClick, handleEditClick, isEditing, editedUser}) => {
   return (
