7786d68b65fa524956976bb060bf796d233f622f
Egglol1 <24egglal@gmail.com>
2025-01-25 13:36:14 -0500

    Fixed filtered MovieCards not having defined functions

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,58 +1,65 @@
-import { useState, useEffect } from "react";
-import { MovieCard } from "../movie-card/movie-card";
-import { MovieView } from "../movie-view/movie-view";
-import { LoginView } from "../login-view/login-view";
-import { SignupView } from "../signup-view/signup-view";
-import { NavigationBar } from "../navigation-bar/navigation-bar";
-import { ProfileView } from "../profile-view/profile-view";
-import {Col, Row, Form, InputGroup} from 'react-bootstrap';
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import { MovieCard } from '../movie-card/movie-card';
+import { MovieView } from '../movie-view/movie-view';
+import { LoginView } from '../login-view/login-view';
+import { SignupView } from '../signup-view/signup-view';
+import { NavigationBar } from '../navigation-bar/navigation-bar';
+import { ProfileView } from '../profile-view/profile-view';
+import { Col, Row, Form, InputGroup } from 'react-bootstrap';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 export const MainView = () => {
-  console.log(localStorage.getItem("user"));
-  const storedUser = JSON.parse(localStorage.getItem("user"));
-  const storedToken = localStorage.getItem("token");
+  console.log(localStorage.getItem('user'));
+  const storedUser = JSON.parse(localStorage.getItem('user'));
+  const storedToken = localStorage.getItem('token');
   const [movies, setMovies] = useState([]);
   const [favoriteMovies, setFavoriteMovies] = useState([]);
   const [user, setUser] = useState(storedUser);
   const [token, setToken] = useState(storedToken);
-  const [filter, setFilter] = useState("");
+  const [filter, setFilter] = useState('');
 
   const handleAddFavorite = (movieId) => {
-    fetch(`https://movie-api-x3ci.onrender.com/user/${user.Username}/movies/${movieId}`, {
-      method: "POST",
-      headers: { 
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}` },
-    })
-    .then((response) => response.json())
-    .then((data) => {
-      setFavoriteMovies(data.FavoriteMovies || []);
-    })
-    .catch((error) => console.error("Error adding to favorites", error));
+    fetch(
+      `https://movie-api-x3ci.onrender.com/user/${user.Username}/movies/${movieId}`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setFavoriteMovies(data.FavoriteMovies || []);
+      })
+      .catch((error) => console.error('Error adding to favorites', error));
   };
 
   const handleRemoveFavorite = (movieId) => {
-    fetch(`https://movie-api-x3ci.onrender.com/user/${user.Username}/movies/${movieId}`, {
-      method: "DELETE",
-      headers: {Authorization: `Bearer ${token}` },
-    })
-    .then((response) => response.json())
-    .then((data) => {
-      setFavoriteMovies(data.FavoriteMovies);
-    })
-    .catch((error) => console.error("Error removing from favorites", error));
+    fetch(
+      `https://movie-api-x3ci.onrender.com/user/${user.Username}/movies/${movieId}`,
+      {
+        method: 'DELETE',
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setFavoriteMovies(data.FavoriteMovies);
+      })
+      .catch((error) => console.error('Error removing from favorites', error));
   };
-  
+
   useEffect(() => {
     if (!token) return;
 
-    fetch("https://movie-api-x3ci.onrender.com/movies", {
-      headers: {Authorization: `Bearer ${token}`}
+    fetch('https://movie-api-x3ci.onrender.com/movies', {
+      headers: { Authorization: `Bearer ${token}` },
     })
       .then((response) => response.json())
       .then((data) => {
-        console.log("Data: ", data)
+        console.log('Data: ', data);
         const moviesFromApi = data.map((movie) => {
           return {
             id: movie._id,
@@ -79,36 +86,36 @@ export const MainView = () => {
   const handleFilterChange = (e) => {
     setFilter(e.target.value);
   };
-  
+
   const filteredMovies = movies.filter((movie) =>
     movie.title.toLowerCase().includes(filter.toLowerCase())
   );
 
-    return (
-      <BrowserRouter>
-        <NavigationBar
-          user={user}
-          onLoggedOut={() => {
-            setUser(null);
-          }}
-        />
-        <Row>
-          <Routes>
-            <Route
-              path="/signup"
-              element={
-                <>
+  return (
+    <BrowserRouter>
+      <NavigationBar
+        user={user}
+        onLoggedOut={() => {
+          setUser(null);
+        }}
+      />
+      <Row>
+        <Routes>
+          <Route
+            path="/signup"
+            element={
+              <>
                 {user ? (
                   <Navigate to="/" />
                 ) : (
                   <Col md={5}>
-                    <SignupView/>
+                    <SignupView />
                   </Col>
                 )}
-                </>
-              }
-            />
-            <Route
+              </>
+            }
+          />
+          <Route
             path="/login"
             element={
               <>
@@ -116,26 +123,27 @@ export const MainView = () => {
                   <Navigate to="/" />
                 ) : (
                   <Col md={5}>
-                    <LoginView onLoggedIn={(user, token) => {
-                      setUser(user);
-                      setToken(token);
-                      }} />
+                    <LoginView
+                      onLoggedIn={(user, token) => {
+                        setUser(user);
+                        setToken(token);
+                      }}
+                    />
                   </Col>
                 )}
               </>
-
             }
-            />
-            <Route
-              path="/"
-              element={
-                <>
-                  <Form className="filter-form mb-4">
-                    <Form.Group controlId="filter">
-                      <Form.Label className="form-label-dark"></Form.Label>
-                      <InputGroup>
+          />
+          <Route
+            path="/"
+            element={
+              <>
+                <Form className="filter-form mb-4">
+                  <Form.Group controlId="filter">
+                    <Form.Label className="form-label-dark"></Form.Label>
+                    <InputGroup>
                       <InputGroup.Text>
-                      <i className="bi bi-search"></i>{" "}
+                        <i className="bi bi-search"></i>{' '}
                       </InputGroup.Text>
                       <Form.Control
                         type="text"
@@ -144,45 +152,50 @@ export const MainView = () => {
                         onChange={handleFilterChange}
                         className="form-control-dark"
                       />
-                      </InputGroup>
-                    </Form.Group>
-                  </Form>
-                  <Row>
-                    {filteredMovies.map((movie) => (
-                      <Col
-                        classNmae="mb-5"
-                        key={movie.id}
-                        xs={12}
-                        sm={6}
-                        md={4}
-                        lg={3}
-                      >
-                        <MovieCard movie={movie}/>
-                      </Col>
-                    ))}
-                  </Row>
-                </>
-              }
-            />
+                    </InputGroup>
+                  </Form.Group>
+                </Form>
+                <Row>
+                  {filteredMovies.map((movie) => (
+                    <Col
+                      classNmae="mb-5"
+                      key={movie.id}
+                      xs={12}
+                      sm={6}
+                      md={4}
+                      lg={3}
+                    >
+                      <MovieCard
+                        movie={movie}
+                        handleAddFavorite={handleAddFavorite}
+                        handleRemoveFavorite={handleRemoveFavorite}
+                        isFavorite={favoriteMovies.includes(String(movie._id))}
+                      />
+                    </Col>
+                  ))}
+                </Row>
+              </>
+            }
+          />
           <Route
             path="movies/:movieId"
             element={
               <>
-              {!user ? (
-                <Navigate to="/login" replace />
-              ) : movies.length === 0? (
-                <Col>The list is Empty!</Col>
-              ) : (
-                <Col md={8}>
-                  <MovieView 
-                  movie={movies}
-                  getSimilarMovies={getSimilarMovies}
-                  user={user}
-                  setUser={setUser}
-                  token={token}
-                  />
-                </Col>
-              )}
+                {!user ? (
+                  <Navigate to="/login" replace />
+                ) : movies.length === 0 ? (
+                  <Col>The list is Empty!</Col>
+                ) : (
+                  <Col md={8}>
+                    <MovieView
+                      movie={movies}
+                      getSimilarMovies={getSimilarMovies}
+                      user={user}
+                      setUser={setUser}
+                      token={token}
+                    />
+                  </Col>
+                )}
               </>
             }
           />
@@ -199,7 +212,9 @@ export const MainView = () => {
                     favoriteMovies={favoriteMovies}
                     setFavoriteMovies={setFavoriteMovies}
                     movies={movies}
-                    setUser={(updatedUser) => dispatchEvent(setUser(updatedUser))}
+                    setUser={(updatedUser) =>
+                      dispatchEvent(setUser(updatedUser))
+                    }
                   />
                 </Col>
               )
@@ -209,29 +224,31 @@ export const MainView = () => {
             path="/"
             element={
               <>
-              {!user ? (
-                <Navigate to="/login" replace />
-              ) : movies.length === 0 ? (
-                <Col>The list is empty!</Col>
-              ) : (
-                <>
-                  {movies.map((movie) => (
-                    <Col className="mb-4" key={movie.id} md={3}>
-                      <MovieCard 
-                        movie={movie}
-                        handleAddFavorite={handleAddFavorite}
-                        handleRemoveFavorite={handleRemoveFavorite}
-                        isFavorite={favoriteMovies.includes(String(movie._id))}
-                       />
-                    </Col>
-                  ))}
-                </>
-              )}
+                {!user ? (
+                  <Navigate to="/login" replace />
+                ) : movies.length === 0 ? (
+                  <Col>The list is empty!</Col>
+                ) : (
+                  <>
+                    {movies.map((movie) => (
+                      <Col className="mb-4" key={movie.id} md={3}>
+                        <MovieCard
+                          movie={movie}
+                          handleAddFavorite={handleAddFavorite}
+                          handleRemoveFavorite={handleRemoveFavorite}
+                          isFavorite={favoriteMovies.includes(
+                            String(movie._id)
+                          )}
+                        />
+                      </Col>
+                    ))}
+                  </>
+                )}
               </>
             }
           />
-          </Routes>
-        </Row>
-      </BrowserRouter>
+        </Routes>
+      </Row>
+    </BrowserRouter>
   );
-  };
+};
